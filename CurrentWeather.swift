6ea4e26b57c7324e57e0fff070aa268573f457d1
Heidi Häti <c6hehati@kc408mac02.tamk.ad>
2018-10-03 15:36:01 +0300

    Current weather tab

---
+++ CurrentWeather.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Heidi Häti on 03/10/2018.
+//  Copyright © 2018 Heidi Häti. All rights reserved.
+//
+
+import UIKit
+
+class CurrentWeather: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "Current weather"
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
